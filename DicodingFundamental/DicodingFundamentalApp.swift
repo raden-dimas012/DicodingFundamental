73ae93081640ab1e5d54d556522b1dd2abb41783
raden-dimas012 <dimascode012@gmail.com>
2021-10-07 15:55:37 +0700

    Initial Commit

---
+++ DicodingFundamental/DicodingFundamentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicodingFundamentalApp.swift
+//  DicodingFundamental
+//
+//  Created by Raden Dimas on 07/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DicodingFundamentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
